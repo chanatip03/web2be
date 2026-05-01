8ea9fea5d6cee24ec3a9fd814bad6db11ea1e0c4
Surasan Phothichak <141292276+Surasan01@users.noreply.github.com>
2026-05-01 13:34:11 +0700

    edit_user_admin

--- app/core/user/dto.py
+++ app/core/user/dto.py
@@ -73,6 +73,27 @@ class UpdateStudentRequest(BaseModel):
     student_id: Optional[str] = None
     image_url: Optional[UploadFile] = File(None)
 
+    @classmethod
+    def as_form(
+        cls,
+        first_name: Optional[str] = Form(None),
+        last_name: Optional[str] = Form(None),
+        email: Optional[EmailStr] = Form(None),
+        password: Optional[str] = Form(None),
+        academy: Optional[str] = Form(None),
+        student_id: Optional[str] = Form(None),
+        image_url: Optional[UploadFile] = File(None),
+    ) -> "UpdateStudentRequest":
+        return cls(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            academy=academy,
+            student_id=student_id,
+            image_url=image_url,
+        )
+
 class UpdateTeacherRequest(BaseModel):
     first_name: Optional[str] = None
     last_name: Optional[str] = None
@@ -81,5 +102,26 @@ class UpdateTeacherRequest(BaseModel):
     academy: Optional[str] = None
     image_url: Optional[UploadFile] = File(None)
     is_verify: Optional[bool] = False
+
+    @classmethod
+    def as_form(
+        cls,
+        first_name: Optional[str] = Form(None),
+        last_name: Optional[str] = Form(None),
+        email: Optional[EmailStr] = Form(None),
+        password: Optional[str] = Form(None),
+        academy: Optional[str] = Form(None),
+        image_url: Optional[UploadFile] = File(None),
+        is_verify: Optional[bool] = Form(False),
+    ) -> "UpdateTeacherRequest":
+        return cls(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            academy=academy,
+            image_url=image_url,
+            is_verify=is_verify,
+        )
 
 
