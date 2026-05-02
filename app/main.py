9bf1f757c53597637bcb4968f1033cd8adc43aeb
V0yager <charnatip15@gmail.com>
2026-05-02 16:43:48 +0700

    fear : allow render url in cors

--- app/main.py
+++ app/main.py
@@ -28,7 +28,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "http://127.0.0.1:3000",        
+        "https://web2fe.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
