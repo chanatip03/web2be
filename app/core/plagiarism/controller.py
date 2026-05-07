2c08f9c95439e9c6cdcd1df298e860286b3bf76e
V0yager <charnatip15@gmail.com>
2026-05-08 00:23:52 +0700

    feat : add error handler plagiarismtest

--- app/core/plagiarism/controller.py
+++ app/core/plagiarism/controller.py
@@ -1,15 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from .repository import extract_avg_comparisons, run_jplag_service
 
 router = APIRouter(prefix="/plagiarism", tags=["Plagiarism"])
 
 @router.post("/check")
 def check_plagiarism():
-    jplag_file = run_jplag_service()
-    result = extract_avg_comparisons(jplag_file)
+    try:
+        jplag_file = run_jplag_service()
+        result = extract_avg_comparisons(jplag_file)
 
-    return {
-        "status": "success",
-        "total_comparisons": len(result),
-        "data": result
-    }
+        return {
+            "status": "success",
+            "total_comparisons": len(result),
+            "data": result
+        }
+
+    except Exception as e:
+        import traceback
+
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "error": str(e),
+                "traceback": traceback.format_exc()
+            }
+        )
